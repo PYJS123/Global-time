0605e59bae995d216581b846b72dfae807071d7d
PYJS123 <bibi.abrah@gmail.com>
2021-07-16 11:53:15 +0100

    Start city add and take

---
+++ buttons.js
@@ -0,0 +1,30 @@
+function changeColourDown(id) {
+  let element = document.getElementById(id);
+  if (id == 'addNote') {
+    element.style.backgroundColor = "#c0c0c0";
+  }
+}
+
+function changeColourUp(id) {
+  let element = document.getElementById(id);
+  if (id == 'addNote') {
+    element.style.backgroundColor = "#d0d0d0";
+  }
+}
+
+function changeColourOver(id) {
+  let element = document.getElementById(id);
+  if (id == 'addNote') {
+    element.style.backgroundColor = "#d0d0d0";
+  }
+}
+
+function changeColourOut(id) {
+  let element = document.getElementById(id);
+  if (id == 'addNote') {
+    element.style.backgroundColor = "#f0f0f0";
+  }
+}
+
+function takeCity(city) {}
+function addCity(city) {}
